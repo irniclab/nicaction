f4472ede3dfbe6c31f47be6d8a7d45a321ea0510
git <eae966@gmail.com>
2023-04-03 17:16:52 +0430

    Some Updates.

--- domainAction/domainAction.go
+++ domainAction/domainAction.go
@@ -1,5 +1,5 @@
 package domainAction
 
 func registerDomain(domain string, nicHanle string, period int, ns1 string, ns2 string, preclTRID string, token string) (bool, error) {
-
+	return true, nil
 }
