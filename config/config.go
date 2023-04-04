7a2c27c923a687d361373628288cb91ccdb367de
git <eae966@gmail.com>
2023-04-04 18:56:37 +0430

    Bug fixed.

--- config/config.go
+++ config/config.go
@@ -3,13 +3,32 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 
 	"github.com/irniclab/nicaction/types"
 )
 
+func generateConfigFile(path string) error {
+	defaultConfig := types.Config{
+		EppAddress:    "https://epp.nic.ir",
+		Nichandle:     "rb994-irnic",
+		Token:         "my-token",
+		AuthCode:      "my-authcode",
+		Ns1:           "ns1.iranns.ir",
+		Ns2:           "ns2.iranns.ir",
+		PreClTRID:     "viraarvand",
+		DefaultPeriod: 1,
+	}
+	SaveConfig(path, defaultConfig)
+}
+
 // LoadConfig بارگذاری تنظیمات از فایل
 func LoadConfig(path string) (types.Config, error) {
 	var conf types.Config
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		generateConfigFile(path)
+	}
 
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
