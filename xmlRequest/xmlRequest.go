ce6cf01e499559854d6b55ca12de0b47d669f3e5
git <eae966@gmail.com>
2023-04-05 18:04:09 +0430

    Update for test

--- xmlRequest/xmlRequest.go
+++ xmlRequest/xmlRequest.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -89,7 +88,7 @@ func SendXml(xml string, config types.Config) (string, error) {
 
 func ParseDomainInfoType(xmlContent string) (*types.DomainType, error) {
 	var di nicResponse.DomainWhoisInfoResponse
-	log.Printf("Raw Result is : %s", xmlContent)
+	//log.Printf("Raw Result is : %s", xmlContent)
 	if err := xml.Unmarshal([]byte(xmlContent), &di); err != nil {
 		return nil, err
 	}
